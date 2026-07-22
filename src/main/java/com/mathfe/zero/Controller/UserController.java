cf21be79b58e7e06072b9d900d118373795f1445
mathfe <matheusferreirasjc@gmail.com>
2026-07-21 22:45:29 -0300

    feat: Implemented the controller for user, with create, findAll, find by id, update and delete

---
+++ src/main/java/com/mathfe/zero/Controller/UserController.java
@@ -0,0 +1,47 @@
+package com.mathfe.zero.Controller;
+
+
+import com.mathfe.zero.Entity.User;
+import com.mathfe.zero.Service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        return userService.findUser(id);
+    }
+
+    @GetMapping("list")
+    public List<User> findAll(){
+        return userService.listUsers();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id){
+        userService.deleteUser(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public User update(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+
+    }
+
